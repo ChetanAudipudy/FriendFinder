fcb45083e1edaa4802354cfc25c4292df3bf510a
Chetan Audipudy <chetan.audipudy@gmail.com>
2018-03-10 01:47:35 -0600

    another bug fix

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,9 +8,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 module.exports=function(app){
-app.get('/api/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/home.html'));
-});
+// app.get('/api/', function(req, res) {
+//     res.sendFile(path.join(__dirname, '../public/home.html'));
+// });
 app.use( function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
 });
