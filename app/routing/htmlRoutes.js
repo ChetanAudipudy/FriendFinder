424ade16e6acf02df14573f06d74a7cd1039f64d
Chetan Audipudy <chetan.audipudy@gmail.com>
2018-03-10 01:45:09 -0600

    app.use method

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -11,6 +11,9 @@ module.exports=function(app){
 app.get('/api/', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/home.html'));
 });
+app.use( function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+});
 
 app.get("/api/survey", function(req, res) {
     res.sendFile(path.join(__dirname, '../public/survey.html'));
